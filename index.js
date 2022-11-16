65bfb2b0d24c14f2f39c52aa653d53e08c8809b9
Tekyto <Tekyto2014@gmail.com>
2022-11-16 14:51:14 +0200

    variables

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function sumOfAreas(squareSide, rectSideA, rectSideB, circleRadius, ellipseMajorAxis, ellipseMinorAxis) {
-	// write your code here
+	const SSquare = squareSide ** 2;
+	const SRect = rectSideA * rectSideB;
+	const SCircle = Math.PI * circleRadius ** 2;
+	const SEllipse = Math.PI * ellipseMinorAxis * ellipseMajorAxis;
+	const STotal = SSquare + SRect + SCircle + SEllipse;
+	return STotal;
 }
 
 module.exports = sumOfAreas;
